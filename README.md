73e6417109b0ab01a65b263674175d9011d09c0f
yann-catlinn <y.catrilelbun@alumnos.duoc.cl>
2022-05-13 20:23:11 +0200

    first commit to update remote repository hito1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # conecta
+
+## Hito 1 e Hito 2
+
+### Links documents included in 'Hito1'
+
+## Project Description
+
+HTML, CSS and JS (Jquery Library were used to develop 'Conecta' project. This project aims to be a base for the final project for Bootcamp Ruby on Rails.
+I've includes Bootstraps components with (Tooltips) and without (Cards, nav, form, btn) Bootstrap JS and I applied Bootstrap's grid layout. In addition, I've applied some effects that use Jquery as the effect for smooth scroll thats replace smooth scroll that comes by default included for Bootstrap.
+
+## Technologies used in this project
+
+- [Bootstrap CSS Framework](https://getbootstrap.com/): Version 5.1.3
+- [JQuery JS Library](https://jquery.com/): Version 3.6.0
---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(function() {
+    $(window).scroll(function() {
+        var topNav = $(".navbar");
+        if ($(this).scrollTop() > 100) {
+            topNav.css("background", "#000");
+        } else {
+            topNav.css("background", "none");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("a").click(function() {
+        var gato = this.hash;
+
+        $("html, body").animate({
+                scrollTop: $(gato).offset().top - 100,
+            },
+            800
+        );
+    });
+});
+
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+function relocate_home() {
+    location.href = "index.html";
+}
